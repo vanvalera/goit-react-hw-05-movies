747c739deec31aa5ecfbd872b0badc907db6abcd
vanvalera <vmarton777@gmail.com>
2023-06-29 22:19:15 +0300

    123

--- src/pages/Movies/Movies.js
+++ src/pages/Movies/Movies.js
@@ -15,19 +15,17 @@ const Movies = () => {
 
   useEffect(() => {
     const query = searchParams.get('query');
+    if (!query) {
+      return;
+    }
     if (query) {
       setSearchQuery(query);
     }
-  }, [searchParams]);
-
-  const searchMovie = query => {
     setLoading(true);
-
     fetchSearchKeyword(query)
       .then(searchResults => {
         setsearchFilms(searchResults);
         setNoMovieText(searchResults.length === 0);
-        setSearchParams({ query });
       })
       .catch(error => {
         console.log(error);
@@ -35,6 +33,10 @@ const Movies = () => {
       .finally(() => {
         setLoading(false);
       });
+  }, [setSearchParams, searchParams]);
+
+  const searchMovie = query => {
+    setSearchParams({ query });
   };
 
   return (
@@ -57,3 +59,63 @@ const Movies = () => {
 };
 
 export default Movies;
+
+// import { fetchSearchKeyword } from 'services/api';
+// import { useState, useEffect } from 'react';
+// import Loader from 'components/Loader/Loader';
+// import EditorList from 'pages/EditorList/EditorList';
+// import css from './Movies.module.css';
+// import MovieSearchForm from 'components/MovieSearchForm/MovieSeacrhForm';
+// import { useSearchParams } from 'react-router-dom';
+
+// const Movies = () => {
+//   const [searchFilms, setsearchFilms] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [noMovieText, setNoMovieText] = useState(false);
+//   const [searchQuery, setSearchQuery] = useState('');
+//   const [searchParams, setSearchParams] = useSearchParams();
+
+//   useEffect(() => {
+//     const query = searchParams.get('query');
+//     if (query) {
+//       setSearchQuery(query);
+//     }
+//   }, [searchParams]);
+
+//   const searchMovie = query => {
+//     setLoading(true);
+
+//     fetchSearchKeyword(query)
+//       .then(searchResults => {
+//         setsearchFilms(searchResults);
+//         setNoMovieText(searchResults.length === 0);
+//         setSearchParams({ query });
+//       })
+//       .catch(error => {
+//         console.log(error);
+//       })
+//       .finally(() => {
+//         setLoading(false);
+//       });
+//   };
+
+//   return (
+//     <main className={css.main}>
+//       <MovieSearchForm
+//         searchMovie={searchMovie}
+//         searchQuery={searchQuery}
+//         setSearchQuery={setSearchQuery}
+//       />
+
+//       {loading && <Loader />}
+
+//       {noMovieText && (
+//         <p>Sorry. There are no movies with this request. Please try again.</p>
+//       )}
+
+//       {searchFilms && <EditorList films={searchFilms} />}
+//     </main>
+//   );
+// };
+
+// export default Movies;
